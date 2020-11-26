856631d3dc100842331dadd7cafddedd4bae24ff
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-26 19:21:44 +0530

    [Kalyan] inserted values into table

--- Management.cs
+++ Management.cs
@@ -24,6 +24,11 @@ namespace AddressBookLinq
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+            // UC3 Inserting Data into Table
+            table.Rows.Add("kalyan", "goud", "8-47", "nlg", "ts", "535501", "8975596720", "mkh@gmail.com");
+            table.Rows.Add("bhanu", "nunna", "Sun nagar", "warangal", "ts", "546489", "8570456737", "ram@gmail.com");
+            table.Rows.Add("Ravi", "kumar", "Rain colony", "Hyd", "ap", "546362", "9878678593", "ravi@gmail.com");
+            table.Rows.Add("anirudh", "repala", "WhiteField", "Banglore", "Karnataka", "125445", "7206326427", "srinu@gmail.com");
         }
     }
 }
